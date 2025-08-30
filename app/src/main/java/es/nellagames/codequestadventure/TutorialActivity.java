a5439ef305b3b10aa2e6d671dfd7f467ec8446df
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-08-30 19:29:51 +0200

    feat: add styled tutorial pages with images and background colors

--- app/src/main/java/es/nellagames/codequestadventure/TutorialActivity.java
+++ app/src/main/java/es/nellagames/codequestadventure/TutorialActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import es.nellagames.codequestadventure.R;
+
 public class TutorialActivity extends AppCompatActivity {
 
     private TextView tutorialText;
