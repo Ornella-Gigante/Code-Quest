38afc0e0da68bc33ed227fd7f444fd1ffda789c0
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-08-30 19:54:32 +0200

    style: improve natural movement and dispersion of stars in tutorial animated background

--- app/src/main/java/es/nellagames/codequestadventure/StarBackgroundView.java
+++ app/src/main/java/es/nellagames/codequestadventure/StarBackgroundView.java
@@ -1,6 +1,5 @@
 package es.nellagames.codequestadventure;
 
-
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -15,7 +14,8 @@ public class StarBackgroundView extends View {
     private static final int NUM_STARS = 25;
     private float[] starX = new float[NUM_STARS];
     private float[] starY = new float[NUM_STARS];
-    private float[] starSpeed = new float[NUM_STARS];
+    private float[] starSpeedY = new float[NUM_STARS];
+    private float[] starSpeedX = new float[NUM_STARS];
     private float[] starSize = new float[NUM_STARS];
     private Paint starPaint;
     private Random random = new Random();
@@ -24,11 +24,13 @@ public class StarBackgroundView extends View {
         super(context, attrs);
         starPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         starPaint.setColor(Color.WHITE);
+        // Inicializa posiciones y velocidades aleatorias por estrella
         for (int i = 0; i < NUM_STARS; i++) {
-            starX[i] = random.nextInt(1000);
-            starY[i] = random.nextInt(1000);
-            starSpeed[i] = 2f + random.nextFloat() * 2f;
-            starSize[i] = 3f + random.nextFloat() * 4f;
+            starX[i] = random.nextInt(1000); // X aleatoria en ancho virtual
+            starY[i] = random.nextInt(1000); // Y aleatoria para desfasar las iniciales
+            starSpeedY[i] = 2f + random.nextFloat() * 2f; // velocidad vertical distinta
+            starSpeedX[i] = -0.5f + random.nextFloat(); // velocidad horizontal entre -0.5 y +0.5
+            starSize[i] = 2f + random.nextFloat() * 4f;
         }
         startAnimation();
     }
@@ -52,12 +54,19 @@ public class StarBackgroundView extends View {
             int w = getWidth();
             int h = getHeight();
             for (int i = 0; i < NUM_STARS; i++) {
-                starY[i] += starSpeed[i];
+                starY[i] += starSpeedY[i];
+                starX[i] += starSpeedX[i];
+                // Si sale por abajo, reinicializa en X/Y random arriba y con nuevo delta X
                 if (starY[i] > h) {
                     starY[i] = 0;
                     starX[i] = random.nextInt((w > 0) ? w : 1000);
-                    starSize[i] = 2f + random.nextFloat() * 5f;
+                    starSize[i] = 2f + random.nextFloat() * 4f;
+                    starSpeedY[i] = 2f + random.nextFloat() * 2f;
+                    starSpeedX[i] = -0.5f + random.nextFloat();
                 }
+                // Si sale por izquierda/derecha, rebota o reinicia
+                if (starX[i] < 0) starX[i] = w - 1;
+                if (starX[i] > w) starX[i] = 1;
             }
             invalidate();
         });
